38df261034efb92f61a6e961e10785df3348e4f5
jackieypd <13735840129@163.com>
2020-06-13 11:32:17 +0800

    Initial commit

---
+++ app/src/main/java/com/jackieypd/emojiconx/MainActivity.java
@@ -0,0 +1,16 @@
+package com.jackieypd.emojiconx;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.jackieypd.emojiconxdemo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ library/src/main/java/com/jackieypd/emojiconx/EmojiconRecents.java
@@ -0,0 +1,9 @@
+package com.jackieypd.emojiconx;
+
+import android.content.Context;
+
+import com.jackieypd.emojiconx.emoji.Emojicon;
+
+public interface EmojiconRecents {
+    void addRecentEmoji(Context context, Emojicon emojicon);
+}
